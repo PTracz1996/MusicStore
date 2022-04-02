1960356a5e1af5c26b7942176dcf6abcdfdbfcbd
PTracz1996 <traczuu96@gmail.com>
2022-04-02 21:15:31 +0200

    Validacja formularza

--- MusicStore/MusicStore/Models/Album.cs
+++ MusicStore/MusicStore/Models/Album.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace MusicStore.Models
 {
     public class Album
     {
+        [ScaffoldColumn(false)]
         public int AlbumId { get; set; }
+        [DisplayName("Genre")]
         public int GenreId { get; set; }
+        [DisplayName("Artist")]
         public int ArtistId { get; set; }
+        [Required(ErrorMessage="An Album Title is required")]
+        [StringLength(160)]
         public string Title { get; set; }
+        [Range(0.01,100,ErrorMessage = "Price must be between 0.01 and 100")]
         public decimal Price { get; set; }
+        [DisplayName("Album Art Url")]
         public string AlbumArtUrl { get; set; }
-        public Genre Genre { get; set; }
-        public Artist Artist { get; set; }
+        public virtual Genre Genre { get; set; }
+        public virtual Artist Artist { get; set; }
     }
 }
